fd8e541061b77fd59a43c1cd7959dab5150273ea
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-13 09:35:03 -0400

    Add files via upload

---
+++ Codigo/17pts/17pts/Models/ContactGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace _17pts.Models
+{
+   public class ContactGroupCollection : ObservableCollection<Contacts>
+    {
+        public ContactGroupCollection(string key)
+        {
+            Key = key;
+        }
+        public string Key { get; }
+    }
+}
---
+++ Codigo/17pts/17pts/Models/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17pts.Models
+{
+   public class Contacts
+    {
+        public Contacts(string name, string telefono)
+        {
+            Name = name;
+            Telefono = telefono;
+        }
+
+        public string Name { get; }
+        public string Telefono { get; }
+        public string Title => Name;
+        public string Detail => Telefono;
+    }
+}
