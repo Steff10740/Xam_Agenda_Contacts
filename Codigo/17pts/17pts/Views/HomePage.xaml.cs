b9f2cdecdebc234000818e99b6d6bc51c6d5ed19
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-13 09:38:51 -0400

    Add files via upload

---
+++ Codigo/17pts/17pts/Views/HomePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using _17pts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _17pts.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+            BindingContext = new HomeViewModel();
+    }
+}
+}
